8bc271fda12606745922ccaf58b23edfcc95af19
sammoh94 <samarthdps@gmail.com>
2019-12-08 21:09:43 -0800

    Finish part 2

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,12 @@ use std::fs::File;
 use std::io::{BufRead, BufReader, Result};
 use std::path::Path;
 
+fn main() -> Result<()> {
+    let requirement = get_sum_fuel_requirements("input.txt")?;
+    println!("{}", requirement);
+    Ok(())
+}
+
 fn get_sum_fuel_requirements<P>(filename: P) -> Result<f64>
 where
     P: AsRef<Path>,
@@ -14,16 +20,25 @@ where
     for line in reader.lines() {
         if let Ok(module_mass_str) = line {
             let mass: f64 = module_mass_str.trim().parse().expect("Not a valid integer");
-            let requirement = round::floor(mass / 3.0, 0) - 2.0;
-            sum += requirement;
+            sum += compute_module_fuel_needs(mass);
         }
     }
 
     Ok(sum)
 }
 
-fn main() -> Result<()> {
-    let requirement = get_sum_fuel_requirements("input.txt")?;
-    println!("{}", requirement);
-    Ok(())
+fn compute_module_fuel_needs(fuel: f64) -> f64 {
+    let mut sum = 0.0;
+    let mut requirement = compute_requirement(fuel);
+
+    while !(requirement < 0.0) {
+        sum += requirement;
+        requirement = compute_requirement(requirement);
+    }
+
+    sum
+}
+
+fn compute_requirement(fuel: f64) -> f64 {
+    round::floor(fuel / 3.0, 0) - 2.0
 }
